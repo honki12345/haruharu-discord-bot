e47e0aa2f600ce2e09659d79613165e14fd352cd
hookim <hookim@student.42gyeongsan.kr>
2024-12-10 13:50:53 +0900

    기상챌린지 출력시간변경

--- src/utils.ts
+++ src/utils.ts
@@ -87,7 +87,7 @@ const PRINT_MINUTES_CAM_STUDY = 59;
 const HARUHARU_TIMES = new Date('2024-04-06');  // 토요일
 
 // challenge 관련 상수들
-const PRINT_HOURS_CHALLENGE = 11;
+const PRINT_HOURS_CHALLENGE = 13;
 const LATE_RANGE_TIME = 10;
 const ABSENCE_RANGE_TIME = 30;
 const DEFAULT_VACANCES_COUNT = 5;
